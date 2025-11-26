d640932f0e68b7697a48737a2d6de694ad62d1a5
Ibtihel Mhamdi <ibtihel.mhamdi@supcom.tn>
2025-11-26 11:24:36 +0100

    show generated interviews

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,13 +1,15 @@
-import Agent from '@/components/Agent'
-import React from 'react'
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const page = () => {
-  return (
-    <>
-     <h3>Interview Generation</h3>
-     <Agent userName="You" userId="user1" type="generate"/>
-    </>
-  )
-}
+const Page = async () => {
+    const user = await getCurrentUser();
 
-export default page
+    return (
+        <>
+            <h3>Interview Generation</h3>
+            <Agent userName={user?.name} userId={user?.id} type="generate" />
+        </>
+    );
+};
+
+export default Page;
